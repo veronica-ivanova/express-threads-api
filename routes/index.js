88c25330adc5dbedcf88f030b4f1d0765d3f9bd4
veronica-ivanova <veronica.ivanova27@gmail.com>
2025-12-29 21:33:56 +0300

    feat: add follow controller

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
-const { UserController, PostController, CommentController, LikeController } = require('../controllers');
+const { UserController, PostController, CommentController, LikeController, FollowController } = require('../controllers');
 const { authenticateToken } = require('../middleware/auth');
 
 const uploadDestination = 'uploads'
@@ -36,5 +36,9 @@ router.delete('/comments/:id', authenticateToken, CommentController.deleteCommen
 //Роуты лайков
 router.post('/likes', authenticateToken, LikeController.likePost)
 router.delete('/likes/:id', authenticateToken, LikeController.unlikePost)
+
+//Роуты подписок
+router.post('/follow', authenticateToken, FollowController.followUser)
+router.delete('/follow/:id', authenticateToken, FollowController.unfollowUser)
 
 module.exports = router;
