3e86d4c8e2f2cafb4b27d0436626b5f4426837e1
veronica-ivanova <veronica.ivanova27@gmail.com>
2026-01-21 15:32:22 +0300

    refactor: update avatar

--- routes/index.js
+++ routes/index.js
@@ -21,7 +21,7 @@ router.post('/register', UserController.register)
 router.post('/login', UserController.login)
 router.get('/current', authenticateToken, UserController.current)
 router.get('/users/:id', authenticateToken, UserController.getUserById)
-router.put('/users/:id', authenticateToken, UserController.updateUser)
+router.put('/users/:id', authenticateToken, upload.single('avatar'), UserController.updateUser)
 
 //Роуты постов
 router.post('/posts', authenticateToken, PostController.createPost )
